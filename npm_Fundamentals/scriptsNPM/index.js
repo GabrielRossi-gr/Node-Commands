fe35956e310f8236e5f0e443ffa4f1278d445316
ga Rossi <gabrieldesouzarossi123456@gmail.com>
2025-03-07 10:57:48 -0300

    add npm fundamentals

---
+++ npm_Fundamentals/scriptsNPM/index.js
@@ -0,0 +1,11 @@
+const chalk = require("chalk")
+
+//scrip escrito no arquivo package.json na parte scripts
+//rodar: npm run <nomeDoScript>
+
+//exemplo:
+//npm start
+//npm run test
+//npm run test2
+
+console.log("eu fui rodado pelo script" + chalk.green.bold("npm run start"))
